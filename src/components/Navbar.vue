b8b68de09a1f348806fb48c7b490e2f3fb6a8e46
Taha CEKEN <taha-ceken@hotmail.com>
2023-01-14 14:11:04 +0300

    navbar routes and playlist create form

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="py-4 px-2.5 mb-12">
-        <nav class="flex justify-between items-center max-w-screen my-0 mx-auto mx-16">
-            <h1 class="bg-clip-text text-text text-4xl font-bold ">
-                <RouterLink to="/"><img class="max-h-44" src="@/assets/logo.png" alt="logo.png"></RouterLink>
+        <nav class="flex md:flex-row flex-col justify-between items-center max-w-screen my-0 mx-4">
+            <h1 class="bg-clip-text text-text text-4xl font-bold">
+                <RouterLink to="/"><img class="max-h-44  md:-ml-8" src="@/assets/logo.png" alt="logo.png"></RouterLink>
             </h1>
-            <div class="space-x-3">
-                <button>Logout</button>
-                <router-link to="signup" class="btn">Signup</router-link>
-                <router-link to="login" class="btn">Login</router-link>
+            <div class="space-x-3 mb-3">
+                <button v-if="user" @click="handleLogout">Logout</button>
+                <router-link v-if="!user" to="/signup" class="btn">Signup</router-link>
+                <router-link v-if="!user" to="/login" class="btn">Login</router-link>
             </div>
         </nav>
     </div>
@@ -15,7 +15,19 @@
 
 <script setup>
 import { RouterLink } from 'vue-router'
+import useLogout from '@/composables/useLogout'
+import getUser from '@/composables/getUser'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+const { user } = getUser()
+const { logout } = useLogout()
+
+const handleLogout = async () => {
+    await logout()
+    console.log('user logged out')
+    router.push('login')
+}
 </script>
 
 
---
+++ src/views/playlists/CreatePlaylist.vue
@@ -0,0 +1,23 @@
+<template>
+    <form>
+        <h4>Create new Playlist</h4>
+        <input type="text" required placeholder="Playlist title" v-model="data.title">
+        <textarea name="" required placeholder="Playlist description" v-model="data.description" id="" cols="30"
+            rows="10"></textarea>
+
+        <!--* upload playlist image -->
+        <label>Upload playlist cover image</label>
+        <input type="file">
+        <button>Create</button>
+    </form>
+</template>
+
+<script setup >
+import { reactive } from 'vue';
+
+const data = reactive({
+    title: '',
+    description: '',
+})
+
+</script>
