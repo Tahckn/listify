33398ba16162127ad1d4450e02f3ba9e6600dd37
Taha CEKEN <taha-ceken@hotmail.com>
2023-01-14 16:13:07 +0300

    file upload support to firebase

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -1,14 +1,21 @@
 <template>
-    <form>
-        <h4>Create new Playlist</h4>
+    <form @submit.prevent="handleSubmit">
+        <h2>Create new Playlist</h2>
         <input type="text" required placeholder="Playlist title" v-model="data.title">
         <textarea name="" required placeholder="Playlist description" v-model="data.description" id="" cols="30"
             rows="10"></textarea>
 
         <!--* upload playlist image -->
-        <label>Upload playlist cover image</label>
-        <input type="file">
-        <button>Create</button>
+        <div class=" border-[1px] flex flex-col rounded-lg border-items border-opacity-30">
+            <label class="text-center font-light text-sm block mt-7">Upload playlist cover image</label>
+            <input id="inp" accept="image/png, image/jpg, image/gif, image/jpeg"
+                class="text-transparent file:-mb-3 file:mx-auto file:block w-full border-0 text-text font-light"
+                type="file" @change="handleChange">
+            <label class="text-text font-light text-center">{{ data.imageName }}</label>
+        </div>
+        <div class="error"></div>
+
+        <button class="mt-5">Create</button>
     </form>
 </template>
 
@@ -18,6 +25,16 @@ import { reactive } from 'vue';
 const data = reactive({
     title: '',
     description: '',
+    imageName: ''
 })
+
+const handleSubmit = () => {
+    console.log(data.title, data.description)
+}
+
+const handleChange = (e) => {
+    console.log(e.target.files[0].name)
+    data.imageName = e.target.files[0].name
+}
 
 </script>
--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: { include: ['firebase/app', 'firebase/firestore'] },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
